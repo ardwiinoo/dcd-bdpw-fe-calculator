918cf2652d620dee5f696bd831016578ce34db6c
arif <ardwino88@gmail.com>
2022-09-09 18:34:23 +0700

    modif: mengubah method performCalculation()

--- assets/main.js
+++ assets/main.js
@@ -43,19 +43,28 @@ function handleOperator(operator) {
 }
  
 function performCalculation() {
-  if (calculator.firstNumber == null || calculator.operator == null) {
-    alert('Anda belum menetapkan operator');
-    return;
-  }
+   if (calculator.firstNumber == null || calculator.operator == null) {
+       alert("Anda belum menetapkan operator");
+       return;
+   }
  
-  let result = 0;
-  if (calculator.operator === '+') {
-    result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
-  } else {
-    result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
-  }
+   let result = 0;
+   if (calculator.operator === "+") {
+       result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+   } else {
+       result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
+   }
  
-  calculator.displayNumber = result;
+   // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+   const history = {
+       firstNumber: calculator.firstNumber,
+       secondNumber: calculator.displayNumber,
+       operator: calculator.operator,
+       result: result
+   }
+   putHistory(history);
+   calculator.displayNumber = result;
+   renderHistory();
 }
  
 const buttons = document.querySelectorAll('.button');
